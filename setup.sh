ab10b97d0f9c3ddb8ea678ec2c92be4193b43251
Bill Desmarais <bill@where.com>
2013-10-08 11:20:26 -0400

    remove echo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 my_dir=${PWD}
 link_filenames=(.bash_aliases .bash_colors .bash_functions .bash_profile .git .gitconfig .gitmodules sshuttle)
 
-echo $my_dir
 for file in ${link_filenames[*]}; do
 
   ln -sf "${my_dir}/${file}" "$HOME/${file}"
